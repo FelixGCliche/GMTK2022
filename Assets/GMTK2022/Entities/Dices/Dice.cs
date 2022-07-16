8d243b61d848fbbfc2b30d9fd8b44967896b740b
Nicolas-Bisson <nicolasbisson14@gmail.com>
2022-07-16 11:36:26 -0400

    Dice Spin

--- Assets/GMTK2022/Entities/Dices/Dice.cs
+++ Assets/GMTK2022/Entities/Dices/Dice.cs
@@ -1,7 +1,13 @@
+using System.Collections;
 using UnityEngine;
+using DG.Tweening;
 
 public class Dice : MonoBehaviour, IDrag
 {
+    [Header("Sides")]
+    [SerializeField] public Vector3[] sidesAngles;
+
+    [Header("SFX")]
     [SerializeField] public AudioClip[] grabSFXs;
     [SerializeField] public AudioClip[] dropSFXs;
 
@@ -9,34 +15,68 @@ public class Dice : MonoBehaviour, IDrag
     private bool hasBeenDropped = false;
     private bool waitingToStabilize = false;
     private bool isActiveDice = true;
+    private int rollValue = 1;
+    private DiceState diceState = DiceState.SPINNING;
 
     private void Awake()
     {
         diceRigidBody = GetComponent<Rigidbody>();
     }
 
+    private void Start()
+    {
+        Roll();
+    }
+
+    private void Roll()
+    {
+        rollValue = Random.Range(0, sidesAngles.Length) + 1;
+        StartCoroutine(Spin());
+        //transform.rotation = Quaternion.Euler(sidesAngles[rollValue-1]);
+    }
+
+    private IEnumerator Spin()
+    {
+        diceState = DiceState.SPINNING;
+        diceRigidBody.useGravity = false;
+
+        for (int i = 0; i < 3; i++)
+        {
+            transform.DORotate(new Vector3(320, 280 , 300), 0.3f, RotateMode.WorldAxisAdd);
+            yield return new WaitForSeconds(0.25f);
+        }
+        transform.DORotate(sidesAngles[rollValue-1], 0.1f);
+        yield return new WaitForSeconds(0.2f);
+        
+        diceState = DiceState.PICKABLE;
+        diceRigidBody.useGravity = true;
+    }
+
     public void OnStartDrag()
     {
         diceRigidBody.useGravity = false;
+        diceState = DiceState.PICKED;
         SoundManager.Instance.PlaySfx(grabSFXs[Random.Range(0, grabSFXs.Length)], transform.position);
     }
 
     public void OnEndDrag()
     {
         diceRigidBody.useGravity = true;
+        diceState = DiceState.FALLING;
         hasBeenDropped = true;
         //diceRigidBody.velocity = Vector3.zero;
     }
 
     public bool CanBeDragged()
     {
-        return !hasBeenDropped;
+        return diceState == DiceState.PICKABLE;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(hasBeenDropped)
         {
+            diceState = DiceState.FALLEN;
             SoundManager.Instance.PlaySfx(dropSFXs[Random.Range(0, dropSFXs.Length)], transform.position);
             waitingToStabilize = true;
         }
---
+++ Assets/GMTK2022/Entities/Dices/DiceState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DiceState
+{
+    SPINNING,
+    PICKABLE,
+    PICKED,
+    FALLING,
+    FALLEN
+}
