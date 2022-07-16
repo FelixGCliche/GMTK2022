4b1be7338724535e91a831d5637a913b287d5213
Nicolas-Bisson <nicolasbisson14@gmail.com>
2022-07-15 21:29:10 -0400

    Auto Spin

---
+++ Assets/GMTK2022/Logic/Camera/CameraStick.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, 1, 0));
+        
+    }
+}
