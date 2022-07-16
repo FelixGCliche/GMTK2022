58a0265468e20c7fcfb95b77e3938ca08734bf54
Nicolas-Bisson <nicolasbisson14@gmail.com>
2022-07-16 13:50:58 -0400

    Add music manager

--- Assets/GMTK2022/Sound/Controller/SoundInitializer.cs
+++ Assets/GMTK2022/Sound/Controller/SoundInitializer.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class SoundInitializer : MonoBehaviour
 {
+    [SerializeField] private AudioClip sceneMusic;
+
     // Start is called before the first frame update
     private void Start()
     {
         SoundManager.Instance.InitAudioSource();
+        SoundManager.Instance.PlayMusic(sceneMusic);
     }
 }
