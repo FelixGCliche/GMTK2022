5456d4acc9725ab07da5e13782bfd64a1b28b7ff
Gabriel St-Laurent-Recoura <gabriel@recoura.com>
2022-07-17 11:23:20 -0400

    Pause game when in pause menu

--- Assets/GMTK2022/UI/PauseMenu/PauseMenuController.cs
+++ Assets/GMTK2022/UI/PauseMenu/PauseMenuController.cs
@@ -7,16 +7,27 @@ public class PauseMenuController : MonoBehaviour
     [SerializeField] private GameObject pauseMenuPage;
     [SerializeField] private GameObject settingsMenuPage;
 
+    private float baseFixedDeltaTime;
+
+    private void Awake()
+    {
+        baseFixedDeltaTime = Time.fixedDeltaTime;
+    }
+
     public void ShowPauseMenu()
     {
         pauseMenuPage.SetActive(true);
         settingsMenuPage.SetActive(false);
+        Time.timeScale = 0f;
+        Time.fixedDeltaTime = 0f;
     }
 
     public void ClosePauseMenu()
     {
         pauseMenuPage.SetActive(false);
         settingsMenuPage.SetActive(false);
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = baseFixedDeltaTime;
     }
 
     public void TogglePauseMenu()
@@ -47,6 +58,8 @@ public class PauseMenuController : MonoBehaviour
     public void ReturnToMainMenu()
     {
         DiceGameManager.Instance.GameEnded();
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = baseFixedDeltaTime;
         LoadingManager.Instance.ChangeScene("MainMenu");
     }
 }
